4d9234e63ca7daa227364ced490b9d9e35dd9647
haghighi <jaydon.haghighi@gmail.com>
2025-05-10 21:40:14 -0400

    added signup screen and authentication

--- frontend/boredless/app/(tabs)/_layout.tsx
+++ frontend/boredless/app/(tabs)/_layout.tsx
@@ -123,7 +123,7 @@ export default function TabLayout() {
               }}
             >
               <Tabs.Screen
-                name="index"
+                name="home"
                 options={{
                   tabBarIcon: ({ focused }) => (
                     <Image
